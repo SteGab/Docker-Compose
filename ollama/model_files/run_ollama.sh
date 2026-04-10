7499be0f24884332c760f23f8ef29a9810f43c9b
STG <stg>
2026-04-10 15:22:18 +0200

    ollama with open-webui

--- ollama/model_files/run_ollama.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Starting Ollama server..."
-#ollama serve &  # Start Ollama in the background
-echo "Ollama is ready, creating the model..."
-
-ollama create stg-standard -f model_files/Modelfile
-ollama run stg-standard
-#ollama run llama3.2
