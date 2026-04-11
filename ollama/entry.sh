2fab2f828f37f6ea1d8bda5c5065295dffa85272
SG <>
2026-04-11 20:32:48 +0200

    typo fixed

--- ollama/entry.sh
+++ ollama/entry.sh
@@ -8,7 +8,8 @@ echo "Waiting for Ollama server to start..."
 sleep 5
 
 echo "Pulling the smollm:360m model..."
-ollama pull smollm:360m
+#ollama pull smollm:360m
+ollama pull gemma4:26b
 
 echo "Model pulled successfully."
 
